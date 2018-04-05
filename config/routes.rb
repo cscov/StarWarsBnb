6b2d3fd7dc2a384efdd29fb832e1998bbf8a38a3
cscov <carolyn.l.scoville@gmail.com>
2018-04-05 09:09:29 -0700

    Version 3 of wireframes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
    namespace :api, defaults: { format: :json } do
      resources :users, only: [:create]
      resource :session, only: [:new, :create, :destroy]
+     resources :bookings, only: [:update]
    end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
