df6eee0dda1afc0f00fa160079c4a26a905700a5
cscov <carolyn.l.scoville@gmail.com>
2018-04-09 08:55:43 -0700

    Added spots routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
    namespace :api, defaults: { format: :json } do
      resources :users, only: [:create]
      resource :session, only: [:new, :create, :destroy]
+     resources :spots, only: [:index, :show]
      resources :bookings, only: [:update]
    end
 
