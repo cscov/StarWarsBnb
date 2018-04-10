ba3aa9702904c3b9c0b93007dabe75111b25e4de
cscov <carolyn.l.scoville@gmail.com>
2018-04-10 15:22:16 -0700

    Index Item styling

--- frontend/components/spots/spots_index_item.jsx
+++ frontend/components/spots/spots_index_item.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import fontawesome from '@fortawesome/fontawesome';
+import faStar from '@fortawesome/fontawesome-free-solid/faStar';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+
 
 const SpotsIndexItem = (props) => {
   return (
     <li className="index-item">
       <React.Fragment>
         <figure><img src={props.spot.spot_first_photo} /></figure>
-        <span className="rental-type">{props.spot.rental_type}</span>
-          <span className="planet">{props.spot.planet}</span>
+        <ul className="top-line">
+          <li className="rental-type filter">{props.spot.rental_type}</li>
+          <li className="planet filter">{props.spot.planet}</li>
+        </ul>
         <span className="title">{props.spot.title}</span>
-        <span className="daily-rate">{props.spot.daily_rate}</span>
-        <span className="stars">{props.spot.stars}</span>
-        <span className="num_reviews">{props.spot.num_reviews}</span>
+        <span className="daily-rate">${props.spot.daily_rate} per night</span>
+        <div className="rating">
+          <FontAwesomeIcon icon={["fas", "star"]} />
+          <FontAwesomeIcon icon={["fas", "star"]} />
+          <FontAwesomeIcon icon={["fas", "star"]} />
+          <FontAwesomeIcon icon={["fas", "star"]} />
+          <FontAwesomeIcon icon={["fas", "star"]} />
+          <span className="num-reviews">{props.spot.num_reviews}</span>
+        </div>
       </React.Fragment>
     </li>
   );
