2385cf3f7b041aa17bcbea7537d3be716c800390
cscov <carolyn.l.scoville@gmail.com>
2018-04-12 11:20:17 -0700

    Updated booking routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
    namespace :api, defaults: { format: :json } do
-     resources :users, only: [:create]
+     resources :users, only: [:create] do
+       resources :bookings, only: %i(show update edit destroy)
+     end
      resource :session, only: [:new, :create, :destroy]
      resources :spots, only: [:index, :show] do
        resources :photos, only: [:show]
+       resources :bookings, only: [:create]
      end
-     resources :bookings, only: [:show, :create, :update, :edit, :destroy]
    end
    root to: 'static_pages#root'
 
