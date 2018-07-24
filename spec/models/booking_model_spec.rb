98461f25aa979f5d98daa5001a1c4d5438def7db
cscov <carolyn.l.scoville@gmail.com>
2018-07-23 17:03:56 -0700

    Validates all presence validations for booking model

--- spec/models/booking_model_spec.rb
+++ spec/models/booking_model_spec.rb
@@ -2,4 +2,10 @@ require "rails_helper"
 
 RSpec.describe Booking, type: :model do
   it { should validate_presence_of(:reservation_code) }
+  it { should validate_presence_of(:traveler_id) }
+  it { should validate_presence_of(:spot_id) }
+  it { should validate_presence_of(:check_in) }
+  it { should validate_presence_of(:check_out) }
+  it { should validate_presence_of(:total_cost) }
+  it { should validate_presence_of(:trip_status) }
 end
