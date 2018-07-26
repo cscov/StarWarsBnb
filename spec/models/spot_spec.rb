c48b98d43e6478d8aaa6bc9876f3f8c70b859e21
cscov <carolyn.l.scoville@gmail.com>
2018-07-25 22:15:09 -0700

    Passing spot amenities minus category spec

--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -3,6 +3,30 @@ require "byebug" # delete
 RSpec.describe Spot, type: :model do
   context "validations" do
     it { should have_db_index(:host_id) }
+
+    it { should validate_presence_of(:host_id) }
+    it { should validate_presence_of(:rental_type) }
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:planet) }
+    it { should validate_presence_of(:num_guests) }
+    it { should validate_presence_of(:num_bedrooms) }
+    it { should validate_presence_of(:num_baths) }
+    it { should validate_presence_of(:num_beds) }
+    it { should validate_presence_of(:daily_rate) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:basic_amenity_category) }
+    it { should validate_presence_of(:sleeping_arrangements) }
+    it { should validate_presence_of(:house_rules) }
+    it { should validate_presence_of(:cancellation_policy) }
+    it { should validate_presence_of(:getting_around) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:directions) }
+    it { should validate_presence_of(:house_manual) }
+
+    # using `validate_inclusion_of` to assert that a boolean column
+    # allows boolean values and disallows non-boolean ones cannot fully test
+    # this, as boolean columns will automatically convert non-boolean values
+    # to boolean ones
   end
 
   context "associations" do
@@ -36,6 +60,24 @@ RSpec.describe Spot, type: :model do
         expect(main_spot.spot_photo_urls).to eq(['test_photo.png',
                                                  'test_photo2.png',
                                                  'test_photo3.png'])
+      end
+    end
+
+    describe "#.amenities_minus_category" do
+      let(:main_spot) { FactoryBot.build(:spot) }
+      it "returns a list of a spot's amenities" do
+        expect(Spot.amenities_minus_category).to eq(['wifi', 'indoor fireplace', 'tv',
+                                                     'iron', 'essentials', 'heating',
+                                                     'air conditioning',
+                                                     'hot water', 'parking',
+                                                     'hot tub', 'kitchen',
+                                                     'lockbox', 'hangers',
+                                                     'hair dryer', 'shampoo',
+                                                     'fire extinguisher',
+                                                     'carbon monoxide detector',
+                                                     'smoke detector',
+                                                     'first aid kit', 'washer',
+                                                     'private entrance'])
       end
     end
   end
